ea801f6953948eaaded946591212f17faf1004d1
mads hargreave <mads.hargreave@gmail.com>
2021-06-01 09:36:00 +0200

    released

--- packages/runtime-main/lib/runtimeFactory.ts
+++
@@ -1,6 +0,0 @@
-import { CreateRuntimeAttrs } from './runtimeAttrs'
-import { Runtime } from './runtimeTypes'
-
-export function buildRuntime({ ...attrs }: CreateRuntimeAttrs): Runtime {
-    return { ...attrs }
-}
