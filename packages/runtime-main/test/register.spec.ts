affcb9c952e078769a674d2bdb8e975d3a7774ad
mads hargreave <mads.hargreave@gmail.com>
2021-06-01 12:36:32 +0200

    released

--- packages/runtime-main/test/register.spec.ts
+++ packages/runtime-main/test/register.spec.ts
@@ -1,18 +1,34 @@
-import { config as createEvent, runtime } from '../lib'
+import { config as createEvent, Dependency, Event, runtime } from '../lib'
+
+type HttpEvent =
+    | Event<'beforeRequest', { url: string }>
+    | Event<'afterRequest', { url: string }>
+
+export interface Http extends Dependency<'http', HttpEvent> {
+    request: () => { status: number }
+}
+
+const http: Http = {
+    name: 'http' as const,
+    events: createEvent<HttpEvent>(),
+    request: () => ({ status: 200 }),
+}
+
+const actual = runtime({
+    dependencies: [http],
+})
 
 describe('register', () => {
     test('simple', () => {
         expect.hasAssertions()
-        const actual = runtime({
-            dependencies: [
-                {
-                    name: 'http' as const,
-                    events: createEvent(),
-                    request: () => ({ status: 200 }),
-                },
-            ],
-        })
         actual.register(({ http }) => {
+            expect(http.request().status).toBe(200)
+        })
+    })
+    test('events', () => {
+        expect.hasAssertions()
+        actual.register(({ http }) => {
+            http.events.on('beforeRequest', () => undefined)
             expect(http.request().status).toBe(200)
         })
     })
