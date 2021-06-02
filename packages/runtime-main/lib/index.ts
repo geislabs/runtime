ace21a4434a77714e967e10883d95369d09935d5
mads hargreave <mads.hargreave@gmail.com>
2021-06-02 12:38:54 +0200

    released

--- packages/runtime-main/lib/index.ts
+++ packages/runtime-main/lib/index.ts
@@ -5,3 +5,9 @@ export {
     Plugin,
     Context,
 } from '@geislabs/runtime-plugin'
+export {
+    Event,
+    EventEmitter,
+    Events,
+    config as createEmitter,
+} from '@geislabs/runtime-event'
